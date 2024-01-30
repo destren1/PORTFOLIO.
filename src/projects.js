f7f83baafa7acc74a2b860883edd6b8d4308f16e
Ivan Romanovskiy <ivanromanovskiy0412@gmail.com>
2024-01-30 18:13:42 +0300

    changed: switching cards and fixed bugs

---
+++ src/projects.js
@@ -0,0 +1,36 @@
+const projects = [
+  {
+    description:
+      "An interactive platform where users are able to post their photos and give likes to their favorite images.",
+    link: "https://github.com/destren1/mesto-project-ff",
+    alt: "project Mesto",
+    src: "./images/projects/mesto.png",
+  },
+  {
+    description:
+      "A web resource that provides the ability to individually select videos by desired criteria from a suggested list, providing a personalized and efficient content search experience.",
+    link: "https://github.com/destren1/posmotri_v_okno",
+    alt: "project posmotri-v-okno",
+    src: "./images/projects/posmotri-v-okno.png",
+  },
+  {
+    description:
+      "A web page looking at the reasons for lack of concentration and providing tips on how to improve it.",
+    link: "https://github.com/destren1/slozhno-sosredotochitsya",
+    alt: "project slozhno-sosredotochitsa",
+    src: "./images/projects/slozhno-sosredotochitsa.png",
+  },
+  {
+    description:
+      "A webpage of personal reflection on my learning journey, where I share my experiences, describe milestones passed and lessons learned, creating a space for self-discovery and inspiration.",
+    link: "https://github.com/destren1/zakrivayuschiy-teg-f",
+    alt: "project zakrivaushiy-teg",
+    src: "./images/projects/zakrivaushiy-teg.png",
+  },
+  {
+    description: "Landing about the auction.",
+    link: "https://github.com/destren1/ono-tebe-nado?tab=readme-ov-file",
+    alt: "project ono-tebe-nado",
+    src: "./images/projects/ono-tebe-nado.PNG",
+  },
+];
--- src/script.js
+++ src/script.js
@@ -1,13 +1,18 @@
 // переменные
-const card = document.querySelectorAll('.card');
+const card = document.querySelector('.card');
 const controls = document.querySelectorAll('.article-projects__controls');
+const link = card.querySelector('.card__link');
+const image = card.querySelector(".card__image");
+const description = card.querySelector(".card__text");
 let cardIndex = 0;
 
 // функция показа карточки
 const showCard = (index) => {
-	card[cardIndex].classList.remove('card__active');
-	card[index].classList.add('card__active');
-	cardIndex = index;
+	cardIndex = index
+	link.href = projects[cardIndex].link;
+	image.src = projects[cardIndex].src;
+	image.alt = projects[cardIndex].alt;
+	description.textContent = projects[cardIndex].description;
 }
 
 // добавляем переключение карточки по клику
@@ -15,14 +20,14 @@ controls.forEach((buttonControl) =>{
 	buttonControl.addEventListener('click', (evt)=>{
 		if(evt.target.classList.contains('button-next')){
 			let index = cardIndex + 1;
-			if(index >= card.length){
+			if(index >= projects.length){
 				index = 0;
 			}
 			showCard(index)
 		} else if(evt.target.classList.contains('button-prev')){
 			let index = cardIndex - 1;
 			if(index < 0){
-				index = card.length - 1;
+				index = projects.length - 1;
 			}
 			showCard(index)
 		}
